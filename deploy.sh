fa8a7f72bc1bd3a2af35f528bc2c17ed43f40eeb
David Wells <dave@measuringu.com>
2024-01-21 13:44:32 -0700

    fix typo in deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'Deployment'
-git push -f git@github.com:DavidWellsTheDeveloper/davidwellsthedeveloper.github.io.git master: gh-pages
+git push -f git@github.com:DavidWellsTheDeveloper/davidwellsthedeveloper.github.io.git master:gh-pages
 
 cd -
